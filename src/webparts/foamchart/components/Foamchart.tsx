7899a701c93c5ad315e859343546545b8277e795
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-02-21 18:48:55 -0500

    Single react webpart that works!

--- src/webparts/foamchart/components/Foamchart.tsx
+++ src/webparts/foamchart/components/Foamchart.tsx
@@ -27,23 +27,23 @@ export default class Foamchart extends React.Component<IFoamchartProps, {}> {
         dataObject: {
           
           groups: [
-            { id: "1", label: "Group 1", groups: [
+            { id: "1", label: "Group 1a", groups: [
               { id: "1.1", label: "Group 1.1" },
               { id: "1.2", label: "Group 1.2" }
             ]},
-            { id: "2", label: "Group 2", groups: [
+            { id: "2", label: "Group 2b", groups: [
               { id: "2.1", label: "Group 2.1" },
               { id: "2.2", label: "Group 2.2" }
             ]},
-            { id: "3", label: "Group 3", groups: [
+            { id: "3", label: "Group 3c", groups: [
               { id: "3.1", label: "Group 3.1" },
               { id: "3.2", label: "Group 3.2" }
             ]},
-            { id: "4", label: "Group 4", groups: [
+            { id: "4", label: "Group 4d", groups: [
               { id: "4.1", label: "Group 4.1" },
               { id: "4.2", label: "Group 4.2" }
             ]},
-            { id: "5", label: "Group 5", groups: [
+            { id: "5", label: "Group 5e", groups: [
               { id: "5.1", label: "Group 5.1" },
               { id: "5.2", label: "Group 5.2" }
             ]}
