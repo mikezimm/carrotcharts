9791556f0998ebf94fc17f65370739193135ad7f
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-03-03 22:29:39 -0500

    Update FoamFunction.tsx

---
+++ src/webparts/foamchart/components/FoamComponent/FoamFunction.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import styles from './Foamcontrol.module.scss';
+import { IFoamcontrolProps } from './IFoamProps';
+import { IFoamcontrolState } from './IFoamState';
+
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import { FoamTree } from "@carrotsearch/foamtree";
+
+import { IFoamTree, IFoamTreeDataObject } from '@mikezimm/npmfunctions/dist/IFoamTree';
+
+import { IFoamTreeList, IFoamItemInfo } from '../GetListData';
+
+import { getFakeFoamTreeData } from '../FakeFoamTreeData';
+
+
+export const FoamFunction: React.FunctionComponent<IFoamcontrolProps> = (props: IFoamcontrolProps) => {    
+
+    let x = props.WebpartWidth > 0 ? props.WebpartWidth + "px" : "500px";
+    let y = props.WebpartHeight > 0 ? props.WebpartHeight + "px" : "500px";
+
+    let foamtree = new FoamTree( props.foamTreeData );
+
+    return (
+      <div className={ styles.foamchart }>
+        <div className={ styles.container }>
+            <div id='visualization1' style={{height: y, width:  x }}></div>
+            { foamtree }
+        </div>
+      </div>
+    );
+
+}
