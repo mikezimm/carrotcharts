fe59b7e1b7997bd428eb85e8950370a43e20622e
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-03-23 13:50:57 -0400

    Update about to v1.0.0.6

--- src/webparts/foamchart/components/HelpInfo/Content/About.tsx
+++ src/webparts/foamchart/components/HelpInfo/Content/About.tsx
@@ -11,6 +11,7 @@ export function aboutTable() {
         rows: [],
     };
 
+    table.rows.push( ['2021-03-19', '1.0.0.6',    <span>Add pass-down props for data and layout buttons, npmFunctions v1.0.15</span>,                ''] );
     table.rows.push( ['2021-03-19', '1.0.0.5',    <span>Update EarlyAccess and Tricks from npmFunctions v1.0.15</span>,                ''] );
     table.rows.push( ['2021-03-16', '1.0.0.4',    <span>Fix foam Height, update PropPane with styles, options, data choices</span>,                ''] );
     table.rows.push( ['2021-03-14', '1.0.0.2',    <span></span>,                ''] );
